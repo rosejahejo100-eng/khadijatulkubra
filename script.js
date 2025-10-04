549de6d0fa9a340ceb1c31e7868550556d18fc3c
rosejahejo100-eng <rosejahejo100@gmail.com>
2025-10-04 04:13:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+    let text = document.querySelector(".text p");
+
+    text.innerHTML = text.innerText
+      .split("")
+      .map((char, i) =>
+        `<b style="transform:rotate(${i * 7}deg)">${char}</b>`
+      )
+      .join("");
+
+
+
+
